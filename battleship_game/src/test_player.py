ad3ce5afb2050b3d08e99ed92584d2e0dfea2a9e
Evan Almloff <evanalmloff@gmail.com>
2024-09-12 15:19:17 -0500

    add information about the player name and number of ships the player selected

--- battleship_game/src/test_player.py
+++ battleship_game/src/test_player.py
@@ -5,10 +5,18 @@ from player import Player
 # Tests for the board
 class TestPlayerMethods(unittest.TestCase):
     def test_take_smallest_piece(self):
-        player = Player()
+        player = Player("Player 1", 5)
         for x in range(1, 6):
             piece = player.takeSmallestPiece()
             self.assertEqual(piece.shape, [[True for _ in range(x)]])
+
+    def test_player_construction(self):
+        player = Player("Player 1", 3)
+        player.unplacedPieces = [
+            Piece([[True, True, True]]),
+            Piece([[True, True]]),
+            Piece([[True]])
+        ]
 
 if __name__ == '__main__':
     unittest.main()
