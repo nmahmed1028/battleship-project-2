309639f4589bdfad0dc072fd63be133d38db4aec
Nayyir Ahmed <nayyir.ahmed1028@gmail.com>
2024-09-27 00:14:21 -0500

    added easy, medium, and hard ai classes

--- battleship_game/src/ai/ai.py
+++ battleship_game/src/ai/ai.py
@@ -1,12 +1,22 @@
 import random #could use this for easy and medium
-from game_mechanics import attack, place_ships
+from board_mechanics.player import Player
+from board_mechanics.board import Board
+from ..config import SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SIZE, ROWS, COLS, WHITE, BLACK, RED, GREEN, BLUE, GRAY, DARK_GRAY, PLAYER_BOARD_OFFSET_X, OPPONENT_BOARD_OFFSET_X, BOARD_OFFSET_Y, BUTTON_RECT, FONT_NAME, FONT_SIZE, TITLE_FONT_SIZE
 
-class AI:
-    def __init__(self, difficulty) -> None:
-        self.difficulty = difficulty.lower()
-        #initialize AI's player board (place ships randomly but legally)
+class AI(Player): #initialize AI as a Player object so it can play like one
+    def __init__(self, name: str, numberOfShips: int) -> None:
+        super().__init__(name, numberOfShips)
 
-    def difficulty_level(self, player_board): #takes in player's ship board for hard difficulty
+    def attack_pattern(self, opponent_board: Board): #this attack pattern picks a random ship, used for easy + med
+        valid_move = False
+        while not valid_move:
+            x = random.randint(0, COLS - 1)
+            y = random.randint(0, ROWS - 1)
+            if not opponent_board.getTile(x, y).isHit():
+                valid_move = True
+        return x, y
+
+    '''def difficulty_level(self, player_board): #takes in player's ship board for hard difficulty
         if self.difficulty == "easy":
             #select random square and attack
             pass
@@ -20,4 +30,43 @@ class AI:
             #might be easier to go in order of smallest to biggest 
             pass
         else:
-            raise ValueError("Invalid input")
+            raise ValueError("Invalid input")'''
+    
+
+class EasyAI(AI):
+    def __init__(self, numberOfShips: int) -> None:
+        super().__init__("Easy AI", numberOfShips)
+    
+    def attack_pattern(self, opponent_board: Board):
+        return super().attack_pattern(opponent_board)
+
+
+class MediumAI(AI):
+    def __init__(self, name: str, numberOfShips: int) -> None:
+        super().__init__("Medium AI", numberOfShips)
+        self.last_hit = None
+    
+    def attack_pattern(self, opponent_board: Board):
+        if self.last_hit: #if 
+            x, y = self.last_hit
+            for i, j in [(-1, 0), (1, 0), (0, -1), (0, 1)]: #checking orthogonal positions
+                newx, newy = x + i, y + j
+                if 0 <= newx < COLS and 0 <= newy < ROWS and not opponent_board.getTile(newx, newy).isHit(): #if position is valid and not hit
+                    return newx, newy
+        return super().attack_pattern(opponent_board) #else default to normal attack pattern (random attack)
+    
+class HardAI(AI):
+    def __init__(self, name: str, numberOfShips: int, opponent_board: Board) -> None:
+        super().__init__("AI-ham", numberOfShips)
+        self.known_targets = [] #stores locations that have ships in them
+
+        for x in range(ROWS): #iterate through opponent board
+            for y in range(COLS):
+                if opponent_board.getTile(x, y).getPiece(): #check if ship at location
+                    self.known_targets.append((x, y)) #if so, add location to known targets
+    
+    def attack_pattern(self, opponent_board: Board):
+        for x, y in self.known_targets: #only iterates through known targets
+            if not opponent_board.getTile(x, y).isHit(): #if position isn't hit yet
+                return x, y
+        
