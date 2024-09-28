41615ff7b1ad2ebf1730e568c32366b6688c77c9
Nayyir Ahmed <nayyir.ahmed1028@gmail.com>
2024-09-28 18:59:14 -0500

    added condition to check if player is ai, lets it use custom attack pattern if so

--- battleship_game/src/game_mechanics/attack.py
+++ battleship_game/src/game_mechanics/attack.py
@@ -17,6 +17,7 @@ from ..board_mechanics.board import Board # Import the Board class from backend
 from ..board_mechanics.player import Player # Import the Piece class from backend
 from ..config import SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SIZE, ROWS, COLS, WHITE, BLACK, RED, GREEN, BLUE, GRAY, DARK_GRAY, PLAYER_BOARD_OFFSET_X, OPPONENT_BOARD_OFFSET_X, BOARD_OFFSET_Y, BUTTON_RECT, FONT_NAME, FONT_SIZE, TITLE_FONT_SIZE
 from ..ui import switch_player_screen
+from ..ai.ai import AI
 
 
 # Define Attack class
@@ -111,6 +112,20 @@ class Attack:
 
             # Draw scores
             self.draw_scores()
+
+            if isinstance(attacker, AI): #if the attacker is the AI player, attack w/ custom attack pattern
+                x, y = attacker.attack_pattern(defender.board)
+                hit = self.handle_attack(defender.board, (x * self.cell_size + OPPONENT_BOARD_OFFSET_X, y * self.cell_size + BOARD_OFFSET_Y), OPPONENT_BOARD_OFFSET_X, BOARD_OFFSET_Y)
+                if hit:
+                    self.show_popup("ai hit", 1)
+                    if attacker is self.player1:
+                        self.player1_score += 1
+                    else:
+                        self.player2_score += 1
+                    return True
+                else:
+                    self.show_popup("ai miss", 1)
+                    return False
 
             # Handle events
             for event in pygame.event.get():
