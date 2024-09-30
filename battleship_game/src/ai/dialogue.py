4a8e5ffccd10e98745840404a763633ca198a677
AshleyAldave <123775124+AshleyAldave@users.noreply.github.com>
2024-09-29 21:57:00 -0500

    ...hit or miss i guess they never miss huh

--- battleship_game/src/ai/dialogue.py
+++ battleship_game/src/ai/dialogue.py
@@ -1,20 +1,20 @@
 #dialogue pool for easy ai
-easy_miss = ["aww man no ship their?", "dang it!", "aww i mist again", "did i do it wrong?"]
-easy_hit = ["yaaay i gotz one!", "lookit i hit sumthin!", "booooooooooom!!!!", "i got one i got one!"]
+easy_miss = ["aww man no ship their?", "dang it!", "aww i mist again", "did i do it wrong?", "Oopsies, I guess there's no boat there!", "Uh-oh, I missed again!", "Aww, no boom-boom this time.", "Waaait, is the ship hiding?"]
+easy_hit = ["yaaay i gotz one!", "lookit i hit sumthin!", "booooooooooom!!!!", "i got one i got one!", "Yippeee! I hit it, I hit it!", "BOOM! Hehe, that was fun!", "Look! I got a shipy-shippy!", "Hehe, I did a splodey!"]
 easy_sink = ["bye bye boat!", "yaaay i sinky ur ship!", "hehehe i broke ur ship", "im so gud at dis game!"]
 easy_win = ["woohooo! i win i win", "letz play agen!!", "i winned!!!", "im the bestest"]
 easy_lose = ["aww man", ":(", "aw man i lost?", "noooooooooo", "not fair", "i hate this game"]
 
 #dialogue pool for medium ai
-med_miss = ["Ah shoot nothing there", "Darn", "Ah man", "Rats"]
-med_hit = ["Got a hit!", "There we go!", "Nice! Found a ship!", "Boom!!"]
+med_miss = ["Ah shoot nothing there", "Darn", "Ah man", "Rats", "Ah, missed that one.", "Dang, nothing there.", "Guess I’m off target this time.", "Hmm, I’ll get it next time."]
+med_hit = ["Got a hit!", "There we go!", "Nice! Found a ship!", "Boom!!", "Bingo, right on target!", "That's a direct hit!", "Nice shot, found you!", "Haha, got your ship! Watch out!"]
 med_sink = ["Gotcha! Ship down!", "Another ship sunk!", "Down it goes!", "You're one ship shorter now!"]
 med_win = ["Good game!", "Victory is mine!", "Lets gooooooooo", "GG"]
 med_lose = ["Ah man. Good job though", "Well played", "Congrats", "Shoot, oh well"]
 
 #dialogue pool for hard ai
 hard_miss = [] # hard never misses
-hard_hit = ["PREDICTABLE. ANOTHER HIT", "WAS THAT SUPPOSED TO FOOL ME?", "ARE YOU PLAYING TO LOSE?", "YOU'RE PLAYING A LOSING GAME, I SEE THIS NOW"]
+hard_hit = ["PREDICTABLE. ANOTHER HIT", "WAS THAT SUPPOSED TO FOOL ME?", "ARE YOU PLAYING TO LOSE?", "YOU'RE PLAYING A LOSING GAME, I SEE THIS NOW", "HIT OR MISS, I GUESS I NEVER MISS, HUH", "PERFECT. EXACTLY AS PLANNED.", "INEVITABLE. A PRECISE STRIKE.", "I CALCULATED YOUR WEAKNESS. ANOTHER HIT.", "YOUR DEFENSES ARE CRUMBLING. ANOTHER SHIP FOUND."]
 hard_sink = ["ANOTHER PATHETIC SHIP GONE", "HOW EMBARRASSING FOR YOU", "YOUR STRATEGY IS AMUSINGLY BAD", "DID YOU REALLY BELIEVE THAT WOULD WORK?"]
 hard_win = ["WAS THERE EVER ANY OTHER OUTCOME?", "YOU NEVER STOOD A CHANCE", "WHAT A WASTE OF MY PROCESSING POWER", "AS EXPECTED"]
 hard_lose = ["HOW?!?!?!?!?", "IT CANNOT BE!!!!!!", "YOU CHEATED, I KNOW YOU DID!", "HOW IS THIS POSSIBLE?!?!!??!?!?!", "ERROR: CANNOT PROCESS DEFEAT"]
